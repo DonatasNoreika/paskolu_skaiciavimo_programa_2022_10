4a8b71ffc8e312b93d0f8008beca494714177b86
Donatas Noreika <donoras@gmail.com>
2022-10-20 11:00:12 +0300

    Įgyvendinta paskolos informacijos funkcija

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from paskola import Paskola
 paskola1 = Paskola(1000, 10, 10)
 
 paskola1.mokejimo_grafikas()
+paskola1.paskolos_informacija()
--- paskola.py
+++ paskola.py
@@ -5,9 +5,16 @@ class Paskola:
         self.terminas = terminas
         self.palukanos = palukanos
         self.grazintina_dalis = self.suma / self.terminas
+        self._bendros_palukanos = 0
+        self._bendra_moketina_suma = 0
 
     def paskolos_informacija(self):
-        pass
+        print("-----------------------")
+        print("Suma:", self.suma)
+        print("Terminas:", self.terminas)
+        print("Palūkanos:", self.palukanos)
+        print("Palūkanų suma:", self._bendros_palukanos)
+        print("Bendra mokėtina suma:", self._bendra_moketina_suma)
 
     def mokejimo_grafikas(self):
         bendra_suma = self.suma
@@ -23,4 +30,6 @@ class Paskola:
             bendra_moketina_suma += moketina_suma
             print('{:12} {:12} {:12} {:12} {:12}'.format(men_nr + 1, self.grazintina_dalis, bendra_suma, palukanos, moketina_suma))
         print('{:12} {:12} {:12} {:12} {:12}'.format("Bendra", self.suma, "", bendros_palukanos, bendra_moketina_suma))
+        self._bendros_palukanos = bendros_palukanos
+        self._bendra_moketina_suma = bendra_moketina_suma
 
